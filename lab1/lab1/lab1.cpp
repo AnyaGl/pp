af9b44b67f77866cb2a2c8ab48af65fc21d823de
AnyaGl <gladysheva_2000@inbox.ru>
2021-03-18 22:22:33 +0300

    Доработки

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
-#include <windows.h>
-#include <tchar.h>
+#include <mutex>
 #include <string>
+#include <tchar.h>
+#include <windows.h>
+#include <vector>
+
 
 DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 {
-	std::cout << "thread number " << *static_cast<int*>(lpParam) << " is working\n";
+	std::cout << "thread number " + std::to_string(*static_cast<int*>(lpParam)) + " is working\n";
 
 	ExitThread(0);
 }
@@ -19,13 +22,17 @@ int _tmain(int argc, _TCHAR* argv[])
 	int numberOfThreads = std::stoi(argv[1]);
 
 	HANDLE* handles = new HANDLE[numberOfThreads];
+	std::vector<int> data;
 
 	for (int i = 0; i < numberOfThreads; i++)
 	{
-		handles[i] = CreateThread(NULL, 0, &ThreadProc, new int(i), CREATE_SUSPENDED, NULL);
+		data.push_back(i);
+	}
+	for (int i = 0; i < numberOfThreads; i++)
+	{
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, &data[i], CREATE_SUSPENDED, NULL);
 		ResumeThread(handles[i]);
 	}
-
 	WaitForMultipleObjects(numberOfThreads, handles, true, INFINITE);
 
 	return 0;
