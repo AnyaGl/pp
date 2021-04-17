515e6731287e2ec707103a90856dd6f86272cdf1
anna.gladysheva <51699328+AnyaGl@users.noreply.github.com>
2021-04-17 14:22:49 +0300

    Выполнена 6 лабораторная

---
+++ lab6/lab6/LogFileWriter.cpp
@@ -0,0 +1,14 @@
+#include "LogFileWriter.h"
+
+LogFileWriter::LogFileWriter(std::string fileName)
+	: m_output{ fileName }
+{
+}
+
+void LogFileWriter::SaveData(List* data)
+{
+	while (data->GetCount() > 0)
+	{
+		m_output << data->Pop() << std::endl;
+	}
+}
